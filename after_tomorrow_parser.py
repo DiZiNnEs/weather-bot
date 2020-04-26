6686c43bee6810b9fcd07cf36b1b0f251dacb5db
dizinnes <dizinnes228@gmail.com>
2020-04-26 17:14:17 +0600

    weather-bot: updated bot and parsers

---
+++ after_tomorrow_parser.py
@@ -0,0 +1,420 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def aktau_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-aktau-mangistauskoy-oblasti/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на после завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+    '''
+        return result
+
+
+def aktobe_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-aktobe/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def almaty_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-almaty/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def atyrau_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-atyrau/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def zhezkazgan_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-zhezkazgane-dzhezkazgane/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def karaganda_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-karagande/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def kyzylorda_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-kyzylorde/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def kokshetau_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-kokshetau/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def kostanai_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-kostanae/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def nursultan_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-nur-sultane/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def nursultan_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-nur-sultane/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def pavlodar_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-pavlodare/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def petropavlovsk_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-petropavlovske/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def semipalatinsk_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-semipalatinske/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def stepnogorsk_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-stepnogorske/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def taldykorgan_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-taldykorgane/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def temirtau_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-temirtau/kazakhstan/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def oskemen_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-ust-kamenogorske/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
+
+
+def shymkent_after_tomorrow_parser():
+    r = requests.get('https://weather.rambler.ru/v-shymkente/3-days/')
+    html = BeautifulSoup(r.content, 'html.parser')
+
+    for tomorrow in html.select('div._18iJ:nth-child(3)'):
+        night_temperature = tomorrow.select_one('span._3k_D:nth-child(2)').text
+        for determine_temperature in tomorrow.select('div._3wdR:nth-child(2)'):
+            day_temperature = determine_temperature.select_one('span._3k_D').text
+        for wind in tomorrow.select('div._1Y0B.wind'):
+            wind_result = wind.select_one('span._1DZh').text.lower()
+        for precipitation in tomorrow.select('div._1Y0B.precipitationProbability'):
+            preciptation_result = precipitation.select_one('span._1DZh').text.lower()
+        result = f'''
+Прогноз погоды на завтра:
+Температура ночью: {night_temperature}
+Температура днём: {day_temperature}
+Скорость ветра: {wind_result}
+Осадки: {preciptation_result}
+        '''
+        return result
--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import telebot
 import pyowm
 
-import parser
+import tomorrow_parser
+import after_tomorrow_parser
 
 owm = pyowm.OWM('04b131cf413fc3c95dc41cb0d44326d0', language='ru')
 bot = telebot.TeleBot("943137137:AAHUONQt5vh4ACwnsAgIur0IdtKAznbl_II")
@@ -68,75 +69,93 @@ def weather_about(city):
 def city(message):
     if message.text.lower() == 'актау':
         bot.send_message(message.chat.id, weather_about('Актау'))
-        bot.send_message(message.chat.id, parser.aktau_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.aktau_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.aktau_after_tomorrow_parser())
 
     elif message.text.lower() == 'актобе':
         bot.send_message(message.chat.id, weather_about('Актобе'))
-        bot.send_message(message.chat.id, parser.aktobe_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.aktobe_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.aktobe_after_tomorrow_parser())
 
     elif message.text.lower() == 'алматы' or message.text.lower() == 'алмата':
         bot.send_message(message.chat.id, weather_about('Алматы'))
-        bot.send_message(message.chat.id, parser.almaty_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.almaty_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.almaty_after_tomorrow_parser())
 
     elif message.text.lower() == 'атырау':
         bot.send_message(message.chat.id, weather_about('Атырау'))
-        bot.send_message(message.chat.id, parser.atyrau_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.atyrau_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.atyrau_after_tomorrow_parser())
 
     elif message.text.lower() == 'жезказган':
         bot.send_message(message.chat.id, weather_about('Жезказган'))
-        bot.send_message(message.chat.id, parser.zhezkazgan_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.zhezkazgan_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.zhezkazgan_after_tomorrow_parser())
 
     elif message.text.lower() == 'караганда':
         bot.send_message(message.chat.id, weather_about('Караганда'))
-        bot.send_message(message.chat.id, parser.karaganda_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.karaganda_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.karaganda_after_tomorrow_parser())
 
     elif message.text.lower() == 'кызылорда':
         bot.send_message(message.chat.id, weather_about('Кызылорда'))
-        bot.send_message(message.chat.id, parser.kyzylorda_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.kyzylorda_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.kyzylorda_after_tomorrow_parser())
 
     elif message.text.lower() == 'кокшетау':
         bot.send_message(message.chat.id, weather_about('Кокшетау'))
-        bot.send_message(message.chat.id, parser.kokshetau_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.kokshetau_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.kokshetau_after_tomorrow_parser())
 
     elif message.text.lower() == 'костанай':
         bot.send_message(message.chat.id, weather_about('Костанай'))
-        bot.send_message(message.chat.id, parser.kostanai_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.kostanai_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.kostanai_after_tomorrow_parser())
 
     elif message.text.lower() == 'нур-султан' or message.text.lower() == 'астана':
         bot.send_message(message.chat.id, weather_about('Нур-Султан'))
-        bot.send_message(message.chat.id, parser.nursultan_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.nursultan_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.nursultan_after_tomorrow_parser())
 
     elif message.text.lower() == 'павлодар':
         bot.send_message(message.chat.id, weather_about('Павлодар'))
-        bot.send_message(message.chat.id, parser.pavlodar_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.pavlodar_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.pavlodar_after_tomorrow_parser())
 
     elif message.text.lower() == 'петропавловск' or message.text.lower() == 'петропавл':
         bot.send_message(message.chat.id, weather_about('Петропавловск'))
-        bot.send_message(message.chat.id, parser.petropavlovsk_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.petropavlovsk_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.petropavlovsk_after_tomorrow_parser())
 
     elif message.text.lower() == 'семей' or message.text.lower() == 'семипалатинск':
         bot.send_message(message.chat.id, weather_about('Семей'))
-        bot.send_message(message.chat.id, parser.semipalatinsk_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.semipalatinsk_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.semipalatinsk_after_tomorrow_parser())
 
     elif message.text.lower() == 'степногорск':
         bot.send_message(message.chat.id, weather_about('Степногорск'))
-        bot.send_message(message.chat.id, parser.stepnogorsk_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.stepnogorsk_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.stepnogorsk_after_tomorrow_parser())
 
     elif message.text.lower() == 'талдыкорган':
         bot.send_message(message.chat.id, weather_about('Талдыкорган'))
-        bot.send_message(message.chat.id, parser.taldykorgan_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.taldykorgan_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.taldykorgan_after_tomorrow_parser())
 
     elif message.text.lower() == 'темиртау':
         bot.send_message(message.chat.id, weather_about('Темиртау'))
-        bot.send_message(message.chat.id, parser.temirtau_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.temirtau_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.temirtau_after_tomorrow_parser())
 
     elif message.text.lower() == 'усть-каменогорск' or message.text.lower() == 'оскемен':
         bot.send_message(message.chat.id, weather_about('Усть-Каменогорск'))
-        bot.send_message(message.chat.id, parser.oskemen_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.oskemen_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.oskemen_after_tomorrow_parser())
 
     elif message.text.lower() == 'шымкент':
         bot.send_message(message.chat.id, weather_about('Шымкент'))
-        bot.send_message(message.chat.id, parser.shymkent_tomorrow_parser())
+        bot.send_message(message.chat.id, tomorrow_parser.shymkent_tomorrow_parser())
+        bot.send_message(message.chat.id, after_tomorrow_parser.shymkent_after_tomorrow_parser())
 
     else:
         bot.send_message(message.chat.id, 'Извините, я вас не понимаю, введите /help')
